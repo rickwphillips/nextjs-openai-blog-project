cbf71145a782b681e06fcd805334cf8ae7fdde24
Rick Phillips <rickwphillips@gmail.com>
2023-04-01 17:42:32 -0400

    Setup Auth0 + Setup Auth0 account and added local env data + Added withPageAuthRequired() to post/new path + Added email and avatar to logged in index.js + Added image hosting in next.config.js

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,7 +1,20 @@
-export default function NewPost() {
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+
+export default function NewPost(props) {
+  console.log('props:', props);
   return (
     <div>
       <h1>This is the New Post page</h1>
     </div>
   );
 }
+
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    return {
+      props: {
+        customProps: 'Testing custom prop with user data'
+      }
+    }
+  }
+});
