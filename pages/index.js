4e964660cba2bc80a59f7d809539a118ebd4b093
Rick Phillips <rickwphillips@gmail.com>
2023-04-01 15:41:00 -0400

    + Added routes pages

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div />;
+  return (
+    <div>
+      <h1>This is the home page</h1>
+    </div>
+  );
 }
---
+++ pages/token-topup.js
@@ -0,0 +1,7 @@
+export default function TokenTopup() {
+  return (
+    <div>
+      <h1>This is the Token Topup page</h1>
+    </div>
+  );
+}
